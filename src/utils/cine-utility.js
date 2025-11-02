36a466d08e575cb97964a7114acc7da5255471fd
Riasadul Hoq <shaon777@gmail.com>
2025-11-02 00:19:50 +0000

    utility updated

--- src/utils/cine-utility.js
+++ src/utils/cine-utility.js
@@ -1,5 +1,5 @@
 export const getImagePath = (imageName) => {
-  const url = `/src/assets/movie-covers/${imageName}`;
+  const url = `/movie-covers/${imageName}`;
   const base = import.meta.url;
   const path = new URL(url, base).href;
   return path;
